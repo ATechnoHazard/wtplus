f62cd51537881e5777fa8e2b339e811ba56e2efd
ATechnoHazard <amolele@gmail.com>
2019-11-06 16:31:06 +0530

    Initial commit
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

---
+++ app/src/main/java/org/firehound/wtplus/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.firehound.wtplus.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.firehound.wtplus.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
