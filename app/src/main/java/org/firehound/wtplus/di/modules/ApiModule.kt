b1fb90e26824de1899ccb17e60c1f479bb0e288b
ATechnoHazard <amolele@gmail.com>
2019-11-10 13:41:59 +0530

    Use named modules to get instances of retrofit and web clients in koin
    * Also add some explanatory comments
    
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app/src/main/java/org/firehound/wtplus/di/modules/ApiModule.kt
+++ app/src/main/java/org/firehound/wtplus/di/modules/ApiModule.kt
@@ -3,7 +3,10 @@ package org.firehound.wtplus.di.modules
 import org.firehound.wtplus.api.customers.CustomerWebClient
 import org.firehound.wtplus.api.customers.CustomerWebService
 import org.firehound.wtplus.api.getOkHttpClient
+import org.firehound.wtplus.api.product.ProductWebClient
+import org.firehound.wtplus.api.product.ProductWebService
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -12,9 +15,9 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 private const val BASE_URL = "http://ec2-3-87-126-246.compute-1.amazonaws.com:3000/"
 
 val apiModule = module {
-    factory { getOkHttpClient(androidContext()) }
+    factory { getOkHttpClient(androidContext()) } // provides okhttp client
 
-    single {
+    single(named("CUSTOMER_RETROFIT")) {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
@@ -23,6 +26,18 @@ val apiModule = module {
             .build()
     }
 
-    factory { get<Retrofit>().create(CustomerWebService::class.java) }
-    factory { CustomerWebClient(get()) }
+    single(named("PRODUCT_RETROFIT")) {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .client(get())
+            .build()
+    }
+
+    factory { get<Retrofit>(named("CUSTOMER_RETROFIT")).create(CustomerWebService::class.java) } // provides a named instance for Customer
+    factory { get<Retrofit>(named("PRODUCT_RETROFIT")).create(ProductWebService::class.java) } // provides a named instance for Product
+
+    factory { CustomerWebClient(get()) } // provides our final web client for customer
+    factory { ProductWebClient(get()) } // provides our final web client for products
 }
